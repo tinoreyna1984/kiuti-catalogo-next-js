29562c001042b52bcab6e43e6bba79d1e4e24114
Tino Reyna <tinoreyna1984@gmail.com>
2023-02-10 11:10:08 -0500

    logo updated

--- components/header.js
+++ components/header.js
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="d-flex flex-row align-items-center shadow rounded-bottom sticky-top" style={{ backgroundColor: "#f16c94" }}>
-      <img className="rounded-bottom" src="/kiuti-logo.jpg" width={98} height={98} alt="Kiuti" />
+      <img className="rounded-bottom" src="/kiuti-logo-02.jpeg" width={98} height={98} alt="Kiuti" />
       <p className="display-4 text-start" id="title">Kiuti Cases</p>
     </header>
   );
