6249c6755f345afffdd68ffdf9bf9d8384b8b9f4
Tino Reyna <tinoreyna1984@gmail.com>
2023-02-10 11:18:25 -0500

    image resolution fixed

--- components/header.js
+++ components/header.js
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
     <header className="d-flex flex-row align-items-center shadow rounded-bottom sticky-top" style={{ backgroundColor: "#f16c94" }}>
-      <img className="rounded-bottom" src="/kiuti-logo-02.jpeg" width={98} height={98} alt="Kiuti" />
-      <p className="display-4 text-start" id="title">Kiuti Cases</p>
+      <img className="rounded-circle p-2" src="/kiuti-logo-02.jpeg" width={94} height={94} alt="Kiuti" />
+      <p className="display-4 text-start mt-3" id="title">Kiuti Cases</p>
     </header>
   );
 }
